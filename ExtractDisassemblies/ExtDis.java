c1dc5d5a4f68b7917a9c6bae328bb1bc853d2edc
lvr <lvr@CHAOSKISTE.localdomain>
2016-04-01 13:51:08 +0200

    add: OpcodeEncryption - java (eclipse) project to parse, manage & store disassemblies.

---
+++ ExtractDisassemblies/ExtDis.java
@@ -0,0 +1,15 @@
+package extdis;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExtDis {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		DisassemblyParser dp = new DisassemblyParser("D:\\SRC\\Java\\extdis\\src\\vim\\exp\\func_disass.1.dis");
+		
+	}
+
+}
+
